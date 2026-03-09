6d677602d6e3a5e52b17b5891e41e0fd7c8748a5
zivulinaviktoria-cell <zivulinaviktoria@gmail.com>
2026-03-10 00:26:55 +0300

    Error test

--- PRapp.py
+++ PRapp.py
@@ -2,7 +2,7 @@ import pytest
 from PR1 import example
 
 def test_1():
-    assert example(2, 3) == 25
+    assert example(2, 3) == 5
     assert example(1, 3) == 16
     assert example(0, 3) == 9
 
