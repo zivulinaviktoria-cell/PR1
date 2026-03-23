d932dfd626152d28bfb255ef314931452785c27b
zivulinaviktoria-cell <zivulinaviktoria@gmail.com>
2026-03-23 20:18:37 +0300

    change

--- PR1.py
+++ PR1.py
@@ -1,2 +1,2 @@
 def example(a, b):
-    return (a + b) ** 2
+    return (a + b) ** 2 - 2 
--- PRapp.py
+++ PRapp.py
@@ -2,7 +2,7 @@ import pytest
 from PR1 import example
 
 def test_1():
-    assert example(2, 3) == 25
-    assert example(1, 3) == 16
-    assert example(0, 3) == 9
+    assert example(2, 3) == 23
+    assert example(1, 3) == 13
+    assert example(0, 3) == 6
 
